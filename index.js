999f3e287ac22c5ccd5781fdde745d680a686416
Roman_Typylo <romatypylo21@gmail.com>
2023-01-21 23:58:29 +0200

    fix3

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import {registerValidation, loginValidation} from "./validations.js";
 import {handleValidationErrors, checkAuth} from './utils/index.js'
 import * as UserController from "./controllers/UserController.js";
 import cors from 'cors'
-import dotenv from 'dotenv'
 import {
     setTransactionsBot,
     setWalletBot
@@ -36,7 +35,7 @@ mongoose
 
 const app = express();
 
-dotenv.config()
+
 app.use(express.json());
 app.use(cors());
 
